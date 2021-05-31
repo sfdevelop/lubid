48296a198b5b36227d3e94f27139f990c950722b
sfdevelop <sfdevelop747@gmail.com>
2021-05-31 16:04:54 +0300

    модальные окна

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,6 +3,7 @@
 //= ../../../node_modules/bootstrap/js/dist/util.js
 //= ../../../node_modules/bootstrap/js/dist/dropdown.js
 //= ../../../node_modules/bootstrap/js/dist/collapse.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
 
 //= ../../../bower_components/slick-carousel/slick/slick.js
 //= ../../../bower_components/fancybox/dist/jquery.fancybox.js
